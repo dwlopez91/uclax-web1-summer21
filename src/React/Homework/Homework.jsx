6463a9f19c082bb4c4dc3a08c8525a0b5ef1a6fb
David Lopez <dwlopez91@gmail.com>
2021-07-19 20:08:27 -0500

    Essay 3

--- src/React/Homework/Homework.jsx
+++ src/React/Homework/Homework.jsx
@@ -8,14 +8,28 @@ const Homework = () => {
             number={ 1 }
             question='What is the difference between Git, Github and Heroku?'
             >
-            My Answer 1
+            <p>Git is a version control system that is local, meaning only on the computer you're using.</p>
+            <p>Github is a cloud based platform for multiple people to push code and collaborate on a project.</p>
+            <p>Heroku is a cloud based platform for collaboration but also has a server to deploy websites.</p>
             </Essay>
             
             <Essay
             number={ 2 }
             question='Explain the difference between HTML, CSS and JS.'
             >
-            My Answer 1
+            <p>HTML is like the skeleton of a webpage. </p>
+            <p>CSS is the style and features of the website.</p> 
+            <p>JS is concerned with the functions of the webpage/site.</p>
+
+            </Essay>
+
+            <Essay
+            number={ 2 }
+            question='What are Single Page Apps (SPAs)? How do they differ from traditional multi-page Web Sites?'
+            >
+            
+            My Answer
+
             </Essay>
         </div>
     );
