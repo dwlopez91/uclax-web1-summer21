28bb85bef9a4dbcf4c8c6f881e30e63e8b77dc15
David Lopez <dwlopez91@gmail.com>
2021-08-16 22:15:48 -0500

    forms forms

--- src/React/Pages/Contact/ContactForm/ContactForm.jsx
+++ src/React/Pages/Contact/ContactForm/ContactForm.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import UniversalForm from 'React/Components/UniversalForm/UniversalForm';
 import styled from 'styled-components';
 
 const ContactForm = () => {
 
     return (
         <ContactFormStyled className='ContactForm'>
-            ContactForm 
+            <UniversalForm /> 
         </ContactFormStyled>
     );
 }
